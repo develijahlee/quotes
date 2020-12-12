30f5b1710e166d029095eb4d3009aa5943b9f38a
Elijah Lee <develijahlee@gmail.com>
2020-12-12 16:19:23 +0900

    seperate backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.use(function (req, res) {
+  res.status(404).send("Page not found")
+})
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}`);
