e78de329cb1d452994fb21b2fcf89079cf437075
Elijah Lee <develijahlee@gmail.com>
2020-12-19 14:52:37 +0900

    resolve revert conflicts

--- backend/models/Quote.js
+++ backend/models/Quote.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+<<<<<<< HEAD:backend/models/Quote.js
 const QuoteSchema = new Schema({
   content: String,
   author: String
+=======
+const TaskSchema = new Schema({
+  taskName: {
+    type: String,
+    required: true
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  }
+>>>>>>> parent of a92d13f... submit strings to server from input:backend/models/Task.js
 });
 
 module.exports = mongoose.model("Quotes", QuoteSchema);
