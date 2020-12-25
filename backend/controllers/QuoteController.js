240d1e6853366213b37edd18a5e416cf373b607f
Elijah Lee <develijahlee@gmail.com>
2020-12-25 23:29:29 +0900

    add redirect after posting form data

--- backend/controllers/QuoteController.js
+++ backend/controllers/QuoteController.js
@@ -22,7 +22,7 @@ exports.createNewQuote = (req, res) => {
         if (err) {
           res.status(500).send(err);
         }
-        res.status(200).json(quote);
+        res.redirect('/quotes')
       });
     }
   });
